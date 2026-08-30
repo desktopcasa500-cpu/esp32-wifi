689142f030adb0cdf9dac23364ad3e4b9ad3daa2
desktopcasa500-cpu <desktopcasa500@gmail.com>
2026-08-30 13:47:01 -0300

    Expose selected BLE target for other modules

--- bt_scanner.h
+++ bt_scanner.h
@@ -1,3 +1,7 @@
 #pragma once
 #include <Arduino.h>
-void bleScan(uint32_t seconds=5);
+
+void bleScan(uint32_t seconds = 5);
+String bleLastSelectedAddress();
+String bleLastSelectedName();
+bool bleHasSelection();
