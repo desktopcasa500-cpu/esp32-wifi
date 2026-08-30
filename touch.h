9679ad0515722fd837078b46ca1529b937617b8b
desktopcasa500-cpu <desktopcasa500@gmail.com>
2026-08-30 13:56:55 -0300

    Add touch calibration interface

--- touch.h
+++ touch.h
@@ -1,5 +1,17 @@
 #pragma once
 #include <Arduino.h>
+
 enum TouchEvent { TOUCH_NONE, TOUCH_PREV, TOUCH_SELECT, TOUCH_NEXT, TOUCH_BACK };
+
+struct TouchCalibration {
+  int16_t minX;
+  int16_t maxX;
+  int16_t minY;
+  int16_t maxY;
+};
+
 void touchBegin();
 TouchEvent touchRead();
+void touchSetCalibration(const TouchCalibration& calibration);
+TouchCalibration touchGetCalibration();
+bool touchCalibrate(TouchCalibration& calibration);
