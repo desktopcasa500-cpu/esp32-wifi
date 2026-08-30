acff625db7d7458134429ea77ddb5991720f41f5
desktopcasa500-cpu <desktopcasa500@gmail.com>
2026-08-30 13:46:15 -0300

    Expand passive packet monitor statistics

--- packet_monitor.h
+++ packet_monitor.h
@@ -1,5 +1,17 @@
 #pragma once
 #include <Arduino.h>
-void packetMonitorStart(uint8_t channel=0);
+
+struct PacketStats {
+  uint32_t total;
+  uint32_t beacon;
+  uint32_t probe;
+  uint32_t data;
+  uint32_t mgmt;
+  uint32_t control;
+  uint16_t pps;
+};
+
+void packetMonitorStart(uint8_t channel = 0);
 void packetMonitorStop();
 uint32_t packetCount();
+PacketStats packetMonitorStats();
