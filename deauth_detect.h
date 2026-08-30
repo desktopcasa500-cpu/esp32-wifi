efbd49eff3b2dd95e537030b9044bb637f07f6de
desktopcasa500-cpu <desktopcasa500@gmail.com>
2026-08-30 13:46:34 -0300

    Expose passive deauth detector counters

--- deauth_detect.h
+++ deauth_detect.h
@@ -1,4 +1,9 @@
 #pragma once
 #include <Arduino.h>
-void deauthDetectorStart(uint8_t channel=0);
+
+void deauthDetectorStart(uint8_t channel = 0);
 void deauthDetectorStop();
+uint32_t deauthEvents();
+uint32_t disassocEvents();
+int deauthLastRssi();
+uint8_t deauthChannel();
