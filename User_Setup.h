bc5b16dd2bb10081d9b89418ab3b505a7ca071d7
desktopcasa500-cpu <desktopcasa500@gmail.com>
2026-08-30 13:56:26 -0300

    Fix TFT bus configuration for VSPI wiring

--- User_Setup.h
+++ User_Setup.h
@@ -7,5 +7,8 @@
 #define TFT_CS   15
 #define TFT_DC   2
 #define TFT_RST  4
+#define TFT_LED  27
 #define SPI_FREQUENCY 40000000
-#define USE_HSPI_PORT
+#define SPI_READ_FREQUENCY 20000000
+#define SPI_TOUCH_FREQUENCY 2500000
+#define TFT_RGB_ORDER TFT_BGR
