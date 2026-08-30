a9ccb940ceec2b21abe888fba03c8bb2bd6bb0b8
desktopcasa500-cpu <desktopcasa500@gmail.com>
2026-08-30 15:12:15 -0300

    Align UI header with stable input API

--- ui.h
+++ ui.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+#include "buttons.h"
 
 extern TFT_eSPI tft;
 
@@ -9,6 +10,7 @@ void uiSetBrightness(uint8_t value);
 void uiHeader(const String& title, const String& status = "");
 void uiFooter();
 void uiMessage(const String& text, const String& title = "ESP32 / TOOLKIT");
-void uiMenu(const String items[], size_t count, int selected);
+void uiMenu(const char* const items[], size_t count, int selected);
 void uiProgress(const String& title, const String& detail, uint8_t percent);
 void uiToast(const String& text, uint16_t ms = 900);
+ButtonEvent uiReadInput();
