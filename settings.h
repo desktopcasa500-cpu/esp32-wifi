ed3c971f3ccbcea27d604b06ba3402810a1ebc0c
desktopcasa500-cpu <desktopcasa500@gmail.com>
2026-08-30 13:45:02 -0300

    Expand persistent settings model

--- settings.h
+++ settings.h
@@ -1,16 +1,26 @@
 #pragma once
 #include <Arduino.h>
+
 struct Settings {
-  uint16_t wifiScanMs=5000;
-  uint16_t bleScanMs=5000;
-  uint8_t brightness=220;
-  uint8_t defaultChannel=1;
-  bool hiddenScan=true;
-  bool autoRescan=false;
-  bool debugSerial=true;
-  String deviceName="ESP32-WiFi-Toolkit";
+  uint16_t wifiScanMs;
+  uint16_t bleScanMs;
+  uint16_t diagnosticTimeoutMs;
+  uint16_t pingsPerDiagnostic;
+  uint8_t brightness;
+  uint8_t defaultChannel;
+  uint8_t theme;
+  uint8_t language;
+  bool hiddenScan;
+  bool autoRescan;
+  bool activeBleScan;
+  bool sortByRssi;
+  bool bootAnimation;
+  bool debugSerial;
+  String deviceName;
 };
+
 extern Settings settings;
+void settingsDefaults();
 void settingsLoad();
 void settingsSave();
 void settingsReset();
