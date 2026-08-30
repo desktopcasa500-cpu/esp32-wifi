f668cd9f4bbeeb4f0d50279e0546a8751e0f158c
desktopcasa500-cpu <desktopcasa500@gmail.com>
2026-08-30 13:48:59 -0300

    Expose complete selected WiFi metadata

--- wifi_analyzer.h
+++ wifi_analyzer.h
@@ -1,8 +1,17 @@
 #pragma once
 #include <Arduino.h>
 
+struct WifiSelection {
+  String ssid;
+  String bssid;
+  uint8_t channel;
+  int rssi;
+  String security;
+};
+
 void wifiScan(bool showHidden = true);
 void wifiPrintDetails(int index);
+WifiSelection wifiLastSelection();
+bool wifiHasSelection();
 String wifiLastSelectedSsid();
 String wifiLastSelectedBssid();
-bool wifiHasSelection();
